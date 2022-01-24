eca36a3a57a78dce03a7d58b8ed1b9e81b2b5cea
Kiran Joshi <45868481+kiranjoshi893@users.noreply.github.com>
2022-01-24 22:36:10 +0530

    Update Store.js

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,44 +1,14 @@
-// import { combineReducers } from "redux";
-// import { createStore, applyMiddleware, compose } from "redux";
-// import thunkMiddleware from "redux-thunk";
-// import { IncrementReducer } from "../reducers/Reducer";
-
-// const rootReducer = combineReducers({
-//   IncrementStore: IncrementReducer,
-// });
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-// export const allStore = createStore(
-//   rootReducer,
-//   composeEnhancers(applyMiddleware(thunkMiddleware))
-// );
-
-import { createStore, applyMiddleware, compose, combineReducers } from "redux";
-import { persistStore, persistReducer } from "redux-persist"; // imports from redux-persist
-import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+import { combineReducers } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunkMiddleware from "redux-thunk";
-import { IncrementReducer, ListReducer, ProductReducer, TestReducer } from "../reducers/Reducer";
-import { saveState } from "./localStorage";
+import {ListReducer, ProductReducer } from "../reducers/Reducer";
 
-
-const persistConfig = {
-  key: "storeData",
-  storage: storage,
-};
 const rootReducer = combineReducers({
   ProductStore: ProductReducer,
   ListStore: ListReducer,
 });
-const persistedReducer = persistReducer(persistConfig, rootReducer); // create a persisted reducer
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
-  persistedReducer,
-  // applyMiddleware(thunkMiddleware)
+export const allStore = createStore(
+  rootReducer,
   composeEnhancers(applyMiddleware(thunkMiddleware))
 );
-store.subscribe(() => {
-  saveState({
-    todos: store.getState().ProductStore.storeData,
-  });
-});
-const persistor = persistStore(store);
-export { store, persistor };
