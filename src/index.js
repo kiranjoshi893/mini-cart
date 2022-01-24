3a3ea7386cd1f99377f63b720b77f421781644dd
Kiran Joshi <45868481+kiranjoshi893@users.noreply.github.com>
2022-01-24 22:34:52 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,15 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { store, persistor } from './store/Store';
-import { PersistGate } from 'redux-persist/integration/react';
+import {allStore} from "./store/Store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
+    <Provider store={allStore}>
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
